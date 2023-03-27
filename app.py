8b6954ed4e5f930dfe1cb1f46c1644f5301290c5
Ronal <zr33lanor@hotmail.com>
2023-03-26 22:48:28 -0600

    agrega horario gt y estilo

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 from zoomus import ZoomClient
+import requests
+import json
+from datetime import datetime
+from pytz import timezone
 
 app = Flask(__name__)
 
@@ -11,8 +15,10 @@ user_id="rsmejia@oj.gob.gt"
 
 @app.route('/')
 def index():
+
     response = zoom_client.meeting.list(user_id=user_id, type='upcoming')
     meetings_list = []
+    
     if response.status_code == 200:
         try:
             meetings_list = response.json().get('meetings', [])
@@ -20,7 +26,15 @@ def index():
             print('Error al decodificar la respuesta JSON: ', e)
     else:
         print('La llamada a la API de Zoom falló con el código de estado HTTP: ', response.status_code)
-    return render_template('index.html', meetings=meetings_list)
+            
+    return render_template('index.html', meetings=meetings_list, user=user_id)
+
+@app.route('/dark_mode')
+def dark_mode():
+    session['dark_mode'] = not session.get('dark_mode', False)
+    return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
